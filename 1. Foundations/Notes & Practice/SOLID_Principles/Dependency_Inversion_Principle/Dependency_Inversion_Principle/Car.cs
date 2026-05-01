5e1a3b95e9702d38726623605297878c5c583df5
Michael Vu <michaelvu1607@gmail.com>
2026-04-30 23:43:02 -0400

    DIP

---
+++ 1. Foundations/Notes & Practice/SOLID_Principles/Dependency_Inversion_Principle/Dependency_Inversion_Principle/Car.cs
@@ -0,0 +1,17 @@
+﻿namespace Dependency_Inversion_Principle;
+
+public class Car
+{
+    private IEngine _engine;
+
+    public Car(IEngine engine)
+    {
+        _engine = engine;
+    }
+
+    public void StartCar()
+    {
+        _engine.Start();
+        Console.WriteLine("Car Started");
+    }
+}
---
+++ 1. Foundations/Notes & Practice/SOLID_Principles/Dependency_Inversion_Principle/Dependency_Inversion_Principle/IEngine.cs
@@ -0,0 +1,6 @@
+﻿namespace Dependency_Inversion_Principle;
+
+public interface IEngine
+{
+    void Start();
+}
