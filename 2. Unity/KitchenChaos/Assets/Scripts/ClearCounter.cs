8925ddd562d803eb20c9a2a0b16edaf3e2c5cf64
Michael Vu <michaelvu1607@gmail.com>
2026-06-09 00:38:15 -0400

    added player pick up and drop off items on a container and added other ContainerCounter variants

--- 2. Unity/KitchenChaos/Assets/Scripts/ClearCounter.cs
+++ 2. Unity/KitchenChaos/Assets/Scripts/ClearCounter.cs
@@ -7,5 +7,31 @@ public class ClearCounter : BaseCounter
 
     public override void Interact(Player player)
     {
+        if (!HasKitchenObject())
+        {
+            // counter not occupied by item
+            if (player.HasKitchenObject())
+            {
+                // player  carrying item
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
+                // player not carrying item
+            }
+        }
+        else
+        {
+            // counter occupied by item
+            if (player.HasKitchenObject())
+            {
+                // player carrying item
+            }
+            else
+            {
+                // player not carrying item
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 }
